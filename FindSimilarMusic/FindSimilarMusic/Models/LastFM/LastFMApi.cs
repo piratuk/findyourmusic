470facd69616d909538367e4fe448f8681989752
Alexander Belenko <AlexBelenko@outlook.com>
2014-10-09 20:47:12 +0300

    Add frontend.

--- FindSimilarMusic/FindSimilarMusic/Models/LastFM/LastFMApi.cs
+++ FindSimilarMusic/FindSimilarMusic/Models/LastFM/LastFMApi.cs
@@ -10,13 +10,14 @@ namespace FindSimilarMusic.Models.LastFM
     {
         private string _apiRoot = "http://ws.audioscrobbler.com/2.0/";
         private string _getSimilar = "?method=artist.getsimilar&artist={0}&api_key=95ba6f9f2e46a04bbe9ce19633fc47e4&format=json";
+        private string _getInfo = "?method=artist.getinfo&artist={0}&api_key=95ba6f9f2e46a04bbe9ce19633fc47e4&format=json";
 
         private Artist[] GetSimilarArtistsForSingleArtist(Artist inArtist)
         {
             var webClient = new WebClient();
             var response = webClient.DownloadString(_apiRoot + string.Format(_getSimilar, inArtist.Name));
             dynamic result = JsonConvert.DeserializeObject(response);
-            if (result["error"]!=null)
+            if (result["error"] != null)
             {
                 return new Artist[0];
             }
@@ -24,8 +25,13 @@ namespace FindSimilarMusic.Models.LastFM
 
             foreach (var artist in result.similarartists.artist)
             {
-                var newArt = new Artist { Name = artist.name, Url = artist.url, PercentageSimilarity = artist.match, ImageUrl = artist.image[4]["#text"]};
+                var newArt = new Artist { Name = artist.name, Url = artist.url, PercentageSimilarity = artist.match, ImageUrl = artist.image[4]["#text"] };
                 newArt.PercentageSimilarity *= 100;
+                //SO SLOW
+                //dynamic desc =
+                //    JsonConvert.DeserializeObject(
+                //        webClient.DownloadString(_apiRoot + string.Format(_getInfo, inArtist.Name)));
+                //newArt.Description = desc.artist.bio.summary;
                 artists.Add(newArt);
             }
             return artists.ToArray();
--- FindSimilarMusic/FindSimilarMusic/Scripts/Index.js
+++ FindSimilarMusic/FindSimilarMusic/Scripts/Index.js
@@ -11,16 +11,39 @@ $("#addArtistButton").click(function () {
         AddRemoveOnClickEvent($(".artistsList").children().last());
     }
     $("#artistInput").val("").focus();
-
 });
+
 $("#artistInput").keypress(function (e) {
     if (e.which == 13) {
         $("#addArtistButton").click();
     }
 });
 $("#searchButton").click(function () {
-    $(".searchResults").fadeIn();
+    var query = "/Home/GetSimilarArtists?";
+    for (var i = 0; i < $(".artistsList").children().length; i++) {
+        query += "artistName=" + $($(".artistsList").children()[i]).text() + "&";
+    }
+    $.getJSON(query, function (data) {
+        var results = data;
+
+        var row = $(".searchResults").children(".row");
+        for (var i = 0; i < results.length; i++) {
+            var artist = results[i];
+            var at = $("<div></div>", {
+                "class": "col-sm-6 col-md-4"
+            }).appendTo(row);
+            var t = $("<div></div").addClass("thumbnail").appendTo(at);
+            var im = $("<div></div>").addClass("img-wrapper").append("<img src=\"" + artist.ImageUrl + "\" alt=\"...\">").appendTo(t);
+            var c = $("<div class=\"caption\"></div>").appendTo(t);
+            $("<a href=\"" + artist.Url + "\" target=\"_blank\">" + "<h3>" + artist.Name + "</h3>" + "</a>").appendTo(c);
+            $("<p>" + artist.Description + "</p>").appendTo(c);
+            var p = $("<div></div>").addClass("progress").appendTo(t);
+            var pb = $("<div aria-valuenow=" + artist.PercentageSimilarity + ">" + Math.round(artist.PercentageSimilarity) + "</div>").addClass("progress-bar").attr("role", "progressbar").attr("aria-valuemin", "0").attr("aria-valuemax", "100").attr("style", "width: " + artist.PercentageSimilarity + "%; font-size: 72px; line-height: 25px").appendTo(p);
+        }
+        $(".searchResults").fadeIn();
+    });
 });
+
 
 function AddRemoveOnClickEvent(element) {
     $(element).hover(function () {
